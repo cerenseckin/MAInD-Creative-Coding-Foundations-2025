8b46edc7b6aea7fae259908f92751546300d1c4d
cerenseckin <74861696+cerenseckin@users.noreply.github.com>
2025-11-06 20:19:46 +0100

    Delivery Assignment 01

--- Assignments/Assignment_01/README.md
+++ Assignments/Assignment_01/README.md
@@ -12,10 +12,17 @@ Starting from the concept of a pinboard, implement a web page that:
 
 ## Screenshots
 
-![first screenshot](DOC/)
+![first screenshot](DOC/Assignment_01_01.png)
 
-![second screenshot](DOC/)
+![second screenshot](DOC/Assignment_01_02.png)
 
 ## Project Description
 
-....
+This application is a dynamic digital pinboard for saving and organizing structured content. It was originally designed for archiving songs, artists and links but its flexible input fields make it useful for remembering any kind of content, link, or resource.
+
+- Enter a Title, Artist (or any custom label), and a URL link. Click “Remember” to pin your entry.
+- Each pinned item is a clickable hyperlink that opens in a new browser tab.
+- The built-in color picker to personalizes each item’s appearance. 
+- Allows the user to Toggle between List View and Card View
+- The Erase All button clears your archive completely, giving the user a fresh start.
+- The application is responsive, providing 2 different views for different devices.
--- Assignments/Assignment_01/assets/script.js
+++ Assignments/Assignment_01/assets/script.js
@@ -44,7 +44,7 @@ rememberButton.addEventListener('click',()=>{
     console.log('Remember button pressed!');
 
     const newSong = document.createElement('li');
-    newSong.style.margin = "20px 0"; 
+    //newSong.style.margin = "20px 0"; 
     newSong.classList.add('song-list-item');
 
     const card = document.createElement('div');
@@ -63,11 +63,6 @@ rememberButton.addEventListener('click',()=>{
     songLink.href = linkInput.value;
     songLink.target = '_blank';
 
-    // const colorBox = document.createElement('div');
-    // colorBox.style.height = '5px';
-    // colorBox.style.width = '100%';
-    // colorBox.style.backgroundColor = colorPicker.value;
-
     card.style.border = `3px solid ${colorPicker.value}`;
 
     card.appendChild(songTitle);
@@ -84,136 +79,3 @@ rememberButton.addEventListener('click',()=>{
     linkInput.value = '';
 
     });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    // const song = songInput.value;
-    // const artist = artistInput.value;
-    // const color = colorPicker.value;
-
-    // const newSong = document.createElement('li');
-
-
-
-
-    // newSong.classList.add('added-song-list-item');
-
-
-
-
-    
-    
-    // songList.appendChild(newSong);
-
-//})
-
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// let CARD = document.createElement("li");
-
-    // let LINK = document.createElement("a");
-    // let divCONTAINER = document.createElement("div");
-    // let COVER = document.createElement("img");
-    // let songName = document.createElement("h2");
-    // let artistName = document.createElement("h3");
-
-    // CARD.append(LINK);
-
-    
-
-
-
-
-
-   // parentElement.append(document.createElement('div'), 'Hello', document.createElement('span'));
-
-
-
-
-    //const inputValue = songInput.value;
-
-    /////////////
-    // const name = songInput.value.trim();
-    // const artist = artistInput.value.trim();
-    // const link = linkInput.value.trim();
-    // const image = coverInput.files?.[0];
-    // const color = colorPicker.value;
-
-/////////////////
-    // const imageUrl = image ? URL.createObjectURL(image) : '';
-    // const imageHtml = imageUrl ? `<img src="${imageUrl}" alt="${name} Cover" class="cover-image">` : '';
-    
-    
-    // const listElement = document.createElement('li');
-    // listElement.classList.add('added-song-list-item');
-
-    // const anchorElement = document.createElement('a');
-    // anchorElement.href = link || "#" ;
-    // anchorElement.target = "_blank";
-
-    // const cardDiv = document.createElement('div');
-    // cardDiv.classList.add('songname-artist-card');
-
-    // cardDiv.innerHTML = `
-    //     ${imageHtml}
-    //     <h2 class="song-name">${name}</h2>
-    //     <h3 class="artist-name">${artist}</h3>
-    //     <button class="remove-btn">X</button> 
-    // `;
-
-    // listElement.style.borderColor = color;
-    // listElement.style.borderStyle = "solid";
-    // listElement.style.borderWidth = '5px';
-
-    // anchorElement.appendChild(cardDiv);
-    // listElement.appendChild(anchorElement);
-
-    // songList.appendChild(listElement);
-
-    // songInput.value = '';
-    // artistInput.value = '';
-    // linkInput.value = '';
-    // coverInput.value = ''; 
-    // });
-
-////////////////
-
-    //const listElement = document.createElement('li');
-    
-
-
-// ERASE.addEventListener('click', ()=>{
-//     console.log("Erase all pressed!");
-//     songList.innerHTML = "";
-// });
-
